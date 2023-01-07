29509c053719c998c1b47fa99d6e80b99df3da35
davi.ricardo <davi.ricardo@csp.com.br>
2023-01-07 16:00:50 -0300

    feat: use react hook forms and authentication data

--- src/components/forms/input/index.tsx
+++ src/components/forms/input/index.tsx
@@ -1,16 +1,19 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, LegacyRef } from 'react';
+import { UseFormRegister } from 'react-hook-form';
 import * as Style from './styles';
 
-type Props = InputHTMLAttributes<HTMLInputElement> & {
-    label: string;
-    type: string;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+  register: UseFormRegister<any>;
+  required?: boolean;
 }
 
-export const Input = ({ label, type, ...props }: Props) => {
-    return (
-        <Style.InputContainer>
-            <label>{label}</label>
-            <input type={type} />
-        </Style.InputContainer>
-    );
+export const Input = ({ name, label, register, required, ...props }: Props) => {
+  return (
+    <Style.InputContainer>
+      <label>{label}</label>
+      <input {...register(name, { required })} {...props} />
+    </Style.InputContainer>
+  );
 };
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,19 +3,55 @@ import { PrimaryButton } from "../../components/forms/primaryButton";
 import { TitleCard } from "../../components/texts/titleCard";
 import * as Style from "./styles";
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { userAuthentication } from "../../data/apiSimulations/login/loginData";
+import { useState } from "react";
 
+type FormData = {
+  username: string;
+  password: string;
+};
 export const LoginScreen = () => {
+  const { register, handleSubmit } = useForm<FormData>();
   const navigate = useNavigate();
-	return (
-			<Style.Container>
-					<Style.InputsContainer>
-							<TitleCard title="Entrar no sistema" subtitle="Insira seus dados para acessar"/>
-							<div>
-									<Input label="Nome de usuário" type="text"/>
-									<Input label="Senha" type="password" />
-							</div>
-							<PrimaryButton text="Entrar" onClick={() => navigate('/home')}/>
-					</Style.InputsContainer>
-			</Style.Container>
-	);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onSubmit = handleSubmit(data => {
+    setLoading(true);
+    setTimeout(() => {
+      if (
+        userAuthentication.username === data.username &&
+        userAuthentication.password === data.password
+      ) {
+        navigate('/home');
+      } else {
+        alert('Dados inválidos!');
+      }
+      setLoading(false);
+    }, 2000);
+  });
+
+  return (
+    <Style.Container>
+      <Style.InputsContainer>
+        {
+          loading ? (
+            <>
+              <TitleCard title="Verificando dados" subtitle="Por favor, aguarde" />
+              <p>Carregando</p>
+            </>
+          ) : (
+            <>
+              <TitleCard title="Entrar no sistema" subtitle="Insira seus dados para acessar" />
+              <form onSubmit={onSubmit}>
+                <Input label="Nome de usuário" name="username" register={register} required />
+                <Input label="Senha" type="password" name="password" register={register} />
+                <PrimaryButton text="Entrar" type="submit" />
+              </form>
+            </>
+          )
+        }
+      </Style.InputsContainer>
+    </Style.Container>
+  );
 };
