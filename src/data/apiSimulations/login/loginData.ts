f28e12339645862f81d0c0736fd081f1b1bd8a94
davi.ricardo <davi.ricardo@csp.com.br>
2023-01-07 16:00:28 -0300

    feat: add mocked authentication data

---
+++ src/data/apiSimulations/login/loginData.ts
@@ -0,0 +1,11 @@
+export const userAuthentication = {
+  username: 'davi',
+	password: 'senha',
+};
+
+export const userData = {
+  userName: 'davi',
+  name: 'Davi Ricardo',
+	password: 'senha',
+	token: '1234567890abcdefghijklmnopqrstwxyz'
+};
